8639901244de06eb725cde64e2ee37cb124210e6
Val Nunez <val@friendlyhello.com>
2026-05-28 14:21:12 -0700

    CardDisplay.cs to wire up the UI

--- dev_tt/Assets/Scripts/CardDisplay.cs
+++ dev_tt/Assets/Scripts/CardDisplay.cs
@@ -1,7 +1,25 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CardDisplay : MonoBehaviour
 {
    // Expose UI elements in Hierarchy for wiring up 
+   [SerializeField]
+   private Image cardImage;
    
+   [SerializeField]
+   private Image cardElementIcon;
+   
+   [SerializeField]
+   private TMP_Text topRankNumber;
+   
+   [SerializeField] 
+   private TMP_Text leftRankNumber;
+   
+   [SerializeField]
+   private TMP_Text rightRankNumber;
+   
+   [SerializeField]
+   private TMP_Text bottomRankNumber;
 }
